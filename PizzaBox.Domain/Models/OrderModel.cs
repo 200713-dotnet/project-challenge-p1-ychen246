7e46340bf3c4c767666268635d68d55a6bdb0495
yujun chen <ychen246@hawk.iit.edu>
2020-08-05 15:25:19 -0500

    Abstract classes for the models.

---
+++ PizzaBox.Domain/Models/OrderModel.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace PizzaStore.Domain.Models
+{
+	public class OrderModel : AModel
+	{
+		public List<PizzaModel> Pizzas { get; set;}
+
+		//public UserModel user { get; set;} 
+		//public StoreModel store { get; set;}
+		//Might need.
+
+		public decimal OrderPrice {
+			get 
+			{
+				decimal TotalOrderSum = 0;
+				foreach (var p in Pizzas)
+				{
+					TotalOrderSum += p.PizzaPrice;
+				}
+				return TotalOrderSum;
+			}
+		}
+	}
+}
---
+++ PizzaBox.Domain/Models/PizzaModel.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+namespace PizzaStore.Domain.Models
+{
+	public class PizzaModel : AModel
+	{
+		public CrustModel Crust { get; set; }
+		public SizeModel Size { get; set; }
+		public List<ToppingModel> Toppings { get; set; }
+
+		public decimal PizzaPrice
+		{
+			get
+			{
+				decimal ToppingPriceSum = 0;
+				foreach (var t in Toppings)
+				{
+					ToppingPriceSum += t.Price;
+				}
+				return Crust.Price + Size.Price + ToppingPriceSum;
+			}
+		}
+	}
+}
